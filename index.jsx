6372d3935a87d8a2b31cf9c2286133a2142714b0
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-20 10:37:14 -0400

    npm installed ans setup

--- index.jsx
+++ index.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Dice from "./Dice.jsx"
 
 function App() {
   return (
-    <h1>Hello World</h1>
+    <div className = "app-container">
+        <main className = "main-container">
+          <h1 className = "h1">Tenzies</h1>
+          <h2 className = "h2">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</h2>
+          
+        </main>
+    </div>
   )
 }
 
