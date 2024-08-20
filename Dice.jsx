8a207ac683cc06c0c2cdc9b606c32bc20f304361
LucyC-1225 <lucyc1225xx@gmail.com>
2024-08-20 13:31:28 -0400

    implemented holdNumber function

--- Dice.jsx
+++ Dice.jsx
@@ -2,7 +2,11 @@ import React from "react"
 
 export default function Dice(props) {
     return (
-        <button className = "dice-container shadow">
+        <button onClick = {() => props.holdNumber(props.index)} 
+        className = "dice-container shadow" 
+        id = {"button" + props.index}
+        style = {props.styles}
+        >
             <p className = "dice-num">1</p>
         </button>
     );
--- index.jsx
+++ index.jsx
@@ -3,6 +3,57 @@ import ReactDOM from 'react-dom/client';
 import Dice from "./Dice.jsx"
 
 function App() {
+
+  /*
+  Names and functions:
+  holdNumber(), holdIndexesArr (array of indexes that are on hold)
+  numbersArr (array of the current 10 numbers)
+  rollDice()
+  1. holdNumber() function, prop for Dice, used for onclick in Dice
+    - changes color to green
+    - add the corresponding index to holdIndexesArr
+    - changes color to white
+    - remove corresponding index from holdIndexesArr
+  2. rollDice() function, onclick inside App
+    - generates n random numbers from 1-6 where n is 10 minus number
+    of elements in holdIndexesArr
+    - assign a number to each of the indexes in numbersArr that are
+    not in holdIndexesArr
+  3. Dice component props
+    - holdNumber
+    - index
+    - numbersArr
+  */
+  const [styles, setStyles] = React.useState({
+    backgroundColor: "white"
+  })
+  const [isHoldArr, setIsHoldArr] = React.useState([
+    false, false, false, false, false, false, false, false, false, false
+  ])
+
+  function holdNumber(index) {
+    console.log("holdNumber function. Selected index " + index);
+    if (!isHoldArr[index]) {
+      setStyles({
+        backgroundColor: "#59E391"
+      })
+    } else {
+      setStyles({
+        backgroundColor: "white"
+      })
+    }
+    // flip the corresponding isHoldArr element 
+    setIsHoldArr(function (prevIsHoldArr) {
+      // const newArr = prevIsHoldArr.map(function (isHold, i) {
+      //   return index === i ? !isHold : isHold;
+      // });
+      // console.log(newArr)
+      return prevIsHoldArr.map(function (isHold, i) {
+        return index === i ? !isHold : isHold;
+      });
+    })
+  }
+  
   return (
     <div className = "app-container">
         <main className = "main-container">
@@ -11,7 +62,11 @@ function App() {
           <div className = "container">
             <div className="row g-0">
               <div className="col">
-                <Dice />
+                <Dice 
+                holdNumber = {holdNumber}
+                index = {0}
+                styles = {styles}
+                />
               </div>
               <div className="col">
                 <Dice />
